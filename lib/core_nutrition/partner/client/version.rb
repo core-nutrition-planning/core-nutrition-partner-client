7848599e47bf8ad02f4893820d041187a224df79
Nate Klaiber <nate@theklaibers.com>
2022-01-17 14:41:47 -0500

    first commit

---
+++ lib/core_nutrition/partner/client/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module CoreNutrition
+  module Partner
+    module Client
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/core_nutrition/partner/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CoreNutrition::Partner::Client do
+  it "has a version number" do
+    expect(CoreNutrition::Partner::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
