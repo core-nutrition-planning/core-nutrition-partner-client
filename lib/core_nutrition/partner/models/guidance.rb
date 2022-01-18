0853db1df21972dadffff818a10309f5c924ae15
Nate Klaiber <nate@theklaibers.com>
2022-01-18 11:10:13 -0500

    Adding the Guidance Goal Records request/response

--- lib/core_nutrition/partner/models/guidance.rb
+++ lib/core_nutrition/partner/models/guidance.rb
@@ -2,6 +2,9 @@
 require File.expand_path('../guidance/athlete_level_types', __FILE__)
 require File.expand_path('../guidance/athlete_level_type', __FILE__)
 
+require File.expand_path('../guidance/goal_records', __FILE__)
+require File.expand_path('../guidance/goal_record', __FILE__)
+
 module CoreNutrition
   module Partner
     module Models
--- lib/core_nutrition/partner/requests/guidance.rb
+++ lib/core_nutrition/partner/requests/guidance.rb
@@ -1,5 +1,6 @@
 # Framework
 require File.expand_path('../guidance/athlete_level_types', __FILE__)
+require File.expand_path('../guidance/goal_records', __FILE__)
 
 module CoreNutrition
   module Partner
